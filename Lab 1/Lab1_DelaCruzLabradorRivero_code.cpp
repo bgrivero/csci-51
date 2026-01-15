ed0ee6da4fc8ba5615d73f79701b053948d91304
Bon Dela Cruz <justinbondc322@gmail.com>
2026-01-15 21:26:20 +0800

    Deleted unnecessary comments and 'TODO'.

--- Lab 1/Lab1_DelaCruzLabradorRivero_code.cpp
+++ Lab 1/Lab1_DelaCruzLabradorRivero_code.cpp
@@ -4,10 +4,6 @@ using namespace std;
 /* 
   This function checks if the character is Uppercase (0), Lowercase (1), or Neither (2).
   These integer values will be used to verify later on whether or not the character will be increased.
-
-  Sources: 
-  https://www.geeksforgeeks.org/dsa/check-whether-the-given-character-is-in-upper-case-lower-case-or-non-alphabetic-character/
-  https://www.w3schools.com/cpp/cpp_conditions.asp
 */
 int check(char x){
   if (x>='A' && x<='Z'){
@@ -24,8 +20,6 @@ int check(char x){
 /*
   This function fulfills requirement #3.
   The replace function is used to simply replace the characters starting from index 1 (2nd char) with '#'.
-
-  Source: https://www.geeksforgeeks.org/cpp/std-string-replace-in-cpp/
 */
 string censor(string name){
   if (name.length() <= 1) {
@@ -39,8 +33,6 @@ string censor(string name){
   This function fulfills requirement #4.
   It uses the integer output of the check function to decide whether or not to increment the char.
   Since characters can be incremented in C++, it is easier to do so rather than making custom dictionary/array.
-
-  Source: https://www.geeksforgeeks.org/cpp/character-arithmetic-in-cpp/
 */
 string getNextLetterV1(string name){
   int string_length = name.length();
@@ -94,7 +86,7 @@ void getNextLetterV2(char* charP){
 int main(void){
   string name;
   cout << "What is your name?: ";
-  getline(cin, name); // uses getline() method to handle empty strings. https://stackoverflow.com/questions/33686108/how-to-read-an-empty-string-in-c
+  getline(cin, name); // uses getline() method to handle empty strings.
   cout << name << endl;
 
   string censored_name = censor(name);
@@ -108,9 +100,3 @@ int main(void){
   }
   cout << name << endl;
 }
-
-/*
-  TODO: 
-  1) Make test cases for debugging.
-  2) Requirement #5 (though im not sure if that's what I did already).
-*/
